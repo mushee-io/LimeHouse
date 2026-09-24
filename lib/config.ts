a652670c60de2d05f31bc11405fe8748fd6c94a4
mushee <musheelabs@proton.me>
2026-09-24 19:30:25 +0100

    Add Lime B chain configuration

---
+++ lib/config.ts
@@ -0,0 +1,7 @@
+export const ULTRA_TESTNET_RPC =
+  process.env.NEXT_PUBLIC_ULTRA_RPC || "https://test.ultra.eosusa.io";
+
+export const LIMEB_CONTRACT =
+  process.env.NEXT_PUBLIC_LIMEB_CONTRACT || "1aa2aa3aa4wr";
+
+export const ULTRA_NETWORK = "testnet" as const;
