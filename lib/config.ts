91e7aa4933f08131853a398e8fca1b14e868bc2c
mushee <musheelabs@proton.me>
2026-09-24 19:55:32 +0100

    Update Lime B account in lib/config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -2,6 +2,6 @@ export const ULTRA_TESTNET_RPC =
   process.env.NEXT_PUBLIC_ULTRA_RPC || "https://test.ultra.eosusa.io";
 
 export const LIMEB_CONTRACT =
-  process.env.NEXT_PUBLIC_LIMEB_CONTRACT || "1aa2aa3aa4wr";
+  process.env.NEXT_PUBLIC_LIMEB_CONTRACT || ".1aa2aa3aa4wr";
 
 export const ULTRA_NETWORK = "testnet" as const;
