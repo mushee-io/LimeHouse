50565aef6575f8868b5b722b0a8128048f01eb63
mushee <musheelabs@proton.me>
2026-09-25 02:44:43 +0100

    Configure standalone LIME token contract

--- lib/config.ts
+++ lib/config.ts
@@ -5,3 +5,6 @@ export const LIMEB_CONTRACT =
   process.env.NEXT_PUBLIC_LIMEB_CONTRACT || "1aa2aa3aa4wr";
 
 export const ULTRA_NETWORK = "testnet" as const;
+
+export const LIME_TOKEN_CONTRACT =
+  process.env.NEXT_PUBLIC_LIME_TOKEN_CONTRACT || "1aa2aa3aa4ws";
